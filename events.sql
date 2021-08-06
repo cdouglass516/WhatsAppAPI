589490a09951a7380af555ba35b12edef91b2465
Chris Douglass <cdouglass516@gmail.com>
2021-08-05 23:55:49 -0500

    It is the last thing ok

---
+++ events.sql
@@ -0,0 +1,33 @@
+--Select * from Events order by Name
+
+insert Into Events (Name, Description,EventURL,StartDate,EndDate, EventImageURL,UserGroupId    ,LocationId,InActive,UserId,EventTypeId)     
+Values('Store Wide Sale Altr`d State',
+'get up to 50% off just in time for fall.',
+'','2021-08-06 11:30:00.000','2021-08-05 18:00:00.000','https://i.ibb.co/sbV92MG/sale.jpg',0,        23,0 ,         2,         4)
+insert Into Events (Name, Description,EventURL,StartDate,EndDate, EventImageURL,UserGroupId    ,LocationId,InActive,UserId,EventTypeId)     
+Values('Yolan`s August Pasta-Making Class','During this exclusive experience, guests will learn how to make Strozzapreti from scratch, then enjoy a special two-course lunch,','https://www.yolannashville.com/events/pastaclass-july','2021-08-06 12:00:00.000','2021-08-06 14:30:00.000','',0,        20,0 ,         2,         0)
+insert Into Events (Name, Description,EventURL,StartDate,EndDate, EventImageURL,UserGroupId    ,LocationId,InActive,UserId,EventTypeId)     
+Values('Alayna Renae','Alayna Renae is an interdisciplinary composer, vocal artist, 
+community organizer, and activist based in Nashville, TN. Learning from past legends 
+like Nina Simone, Alayna`s music reflects the times we are living through as she sings 
+from personal transformation. ','https://www.eventbrite.com/e/alayna-renae-tickets-164680494793',
+'2021-08-06 17:30:00.000','2021-08-06 19:15:00.000','https://i.ibb.co/wKzfpjF/music.jpg',0,19,0 ,2,1)
+insert Into Events (Name, Description,EventURL,StartDate,EndDate, EventImageURL,UserGroupId    ,LocationId,InActive,UserId,EventTypeId)     
+Values('Magic The Gathering','Join other Magic players for a night of magic.','','2021-08-06 18:00:00.000','2021-08-06 21:30:00.000','https://i.ibb.co/sPvBMn9/bar.jpg',0,        14,0 ,         2,         9)
+insert Into Events (Name, Description,EventURL,StartDate,EndDate, EventImageURL,UserGroupId    ,LocationId,InActive,UserId,EventTypeId)     
+Values('Tim Carroll`s Rock ’n` Roll Happy Hour','Friday’s Tim Carroll Rock’n Roll Happy Hour is FREE ','','2021-08-06 18:00:00.000','2021-08-06 19:30:00.000','https://ucarecdn.com/42d3232f-5d68-4a24-a6de-9024bbaa2412/-/crop/1157x625/5,0/-/resize/500x270/',0,        22,0 ,         2,         8)
+insert Into Events (Name, Description,EventURL,StartDate,EndDate, EventImageURL,UserGroupId    ,LocationId,InActive,UserId,EventTypeId)     
+Values('IN STORE: Jeff Zentner','Parnassus Books is thrilled to welcome Jeff Zentner, YA author of In The Wild Light, in conversation with David Arnold.','https://www.parnassusbooks.net/event/author-event-jeff-zentner-ya-author-wild-light-conversation-david-arnold','2021-08-06 18:30:00.000','2021-08-06 20:00:00.000','https://i.ibb.co/hKnwVmV/speaker.jpg',0,        24,0 ,         2,         5)
+insert Into Events (Name, Description,EventURL,StartDate,EndDate, EventImageURL,UserGroupId    ,LocationId,InActive,UserId,EventTypeId)     
+Values('ROCKNPOD PREPARTY: RESIST & BITE, RARE HARE JAM, RON KEEL, AND ROCK UNITED','ROCKNPOD Weekend kicks off Friday August 6th with the ROCKNPOD Preparty at Mercy Lounge! Join us for a great night of rock music featuring very special guests! Resist & Bite, Rare Hare Jam, Ron Keel, and Rock United','https://www.mercylounge.com/e/rocknpod-preparty-resist-bite-rare-hare-jam-ron-keel-and-rock-united-148663473451/','2021-08-06 18:30:00.000','2021-08-06 19:30:00.000','https://img-dev.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F130594513%2F314933172755%2F1%2Foriginal.20210329-194240?auto=compress&fit=clip&h=&w=650&s=8edd1a124c1c629c78abca6117c117a4',0,        26,0 ,         2,         1)
+insert Into Events (Name, Description,EventURL,StartDate,EndDate, EventImageURL,UserGroupId    ,LocationId,InActive,UserId,EventTypeId)     
+Values('Juliet Hawkins','Juliet Hawkins and her awesome band come to rock the house! $5 Dee`s Country Cocktail Lounge 102 E Palestine Ave, Madison','https://deeslounge.com/shows/list/','2021-08-06 20:00:00.000','2021-08-06 21:30:00.000','https://i.ibb.co/wKzfpjF/music.jpg',0,        28,0 ,         2,         1)
+insert Into Events (Name, Description,EventURL,StartDate,EndDate, EventImageURL,UserGroupId    ,LocationId,InActive,UserId,EventTypeId)     
+Values('Nine Ball championship','Nine Ball tourney with 1500.00 Grand Prize','','2021-08-06 20:15:00.000','2021-08-06 01:15:00.000','https://i.ibb.co/vxrgZLy/sports.jpg',0,        27,0 ,         2,         6)
+insert Into Events (Name, Description,EventURL,StartDate,EndDate, EventImageURL,UserGroupId    ,LocationId,InActive,UserId,EventTypeId)     
+Values('Charlatan Heston','Bowling Green rock heroes invade Tennessee for one night only, beware! $5 Dee`s Country Cocktail Lounge 102 E Palestine Ave, Madison','https://deeslounge.com/event/charlatan-heston/','2021-08-06 22:00:00.000','2021-08-06 01:00:00.000','https://i.ibb.co/wKzfpjF/music.jpg',0,        28,0 ,         2,         1)
+insert Into Events (Name, Description,EventURL,StartDate,EndDate, EventImageURL,UserGroupId    ,LocationId,InActive,UserId,EventTypeId)     
+Values('Dog Nights of summer','We`re going to the dogs! Pack up the whole family and join us for dog-friendly nights in the garden ',
+'https://cheekwood.org/calendar/dog-nights-of-summer/','2021-08-08 19:00:00.000','2021-08-08 21:00:00.000',
+'https://cheekwood.org/wp-content/uploads/2019/04/Dog-Nights-of-Summer-640x524.jpg',0,        18,0 ,         2,         8)
+
---
+++ locations.sql
@@ -0,0 +1,73 @@
+
+insert Into Locations (Name, Description, LocationURL, LatLon,InActive, ImageUrl) 
+Values('Comix City Too!','Comix City Too! is the friendliest, greatest, and most humble store in Middle TN. 
+If you love comics, board games, card games, tabletop games and meeting awesome people that love what you love, 
+we are the place to be.Located in: Rivergate Square Open till 7pm',
+'https://comixcitytoo.com/','POINT (36.291186  -86.704956)', 0 ,'https://comixcitytoo.com/wp-content/uploads/2014/04/Snapcaster-Mage-MtG-Card.jpg')
+
+
+insert Into Locations (Name, Description, LocationURL, LatLon,InActive, ImageUrl) 
+Values('Tempo Nashville','Event Space 2179 Nolensville Pike',
+'','POINT (36.1276319 -86.7564165)', 0 ,'')
+
+insert Into Locations (Name, Description, LocationURL, LatLon,InActive, ImageUrl) 
+Values('Out East Comedy club','
+Owned by comedian Da’Herm we are open to the public with community businesses serving at vendors booths. 
+We offer a wide range of comedy open Mics with an occasional community event. ',
+'https://outeastcomedyclub.com/about-us/','POINT (36.1938278 -86.7456865)', 0 ,
+'https://outeastcomedyclub.com/wp-content/uploads/2020/11/118936217_102083161643015_6696671509836153563_n-800x802.jpg')
+
+
+insert Into Locations (Name, Description, LocationURL, LatLon,InActive, ImageUrl) 
+Values('Brooklyn Bowl Nashville','Eat. Drink. Rock. And Roll. Welcome to Brooklyn Bowl Nashville! Catch a rockin` show, strike down 
+some pins, grab a bite, 
+and sip and chill on our patio. We`ve got a little something for everyone.
+It`s just the beginning. Let`s do this, Music City!',
+'https://www.brooklynbowl.com/nashville','POINT (36.1735577 -86.7861929)', 0 ,'')
+
+insert Into Locations (Name, Description, LocationURL, LatLon,InActive, ImageUrl) 
+Values('Cheekwood','Cheekwood is a 55-acre botanical garden and historic estate in Nashville, featuring art galleries, seasonal festivals and breathtaking wedding venues.',
+'https://cheekwood.org/','POINT (36.0884514 -86.8781636)', 0 ,'https://cheekwood.org/wp-content/uploads/2021/07/Lobelia-cardinalis-1-399x264.jpg')
+
+insert Into Locations (Name, Description, LocationURL, LatLon,InActive, ImageUrl) 
+Values('Rudy`s Jazz Room','809 Gleaves Street, Nashville, TN 37203',
+'https://www.rudysjazzroom.com/','POINT (36.1517995 -86.7823093)', 0 ,'https://www.rudysjazzroom.com/img/Rudys_website_homepage_banner.jpg')
+
+
+insert Into Locations (Name, Description, LocationURL, LatLon,InActive, ImageUrl) 
+Values('Yolan`s','Polished, stylish Italian restaurant specializing in avant-garde entrees, plus a large wine list',
+'https://www.yolannashville.com/','POINT (36.1565655 -86.774718)', 0 ,'')
+
+
+insert Into Locations (Name, Description, LocationURL, LatLon,InActive, ImageUrl) 
+Values('Mercy Lounge','Eclectic mix of local & touring acts hits the small stage of this funky lounge/bar almost nightly',
+'https://www.mercylounge.com/','POINT (36.1529728 -86.7826866)', 0 ,'')
+
+
+insert Into Locations (Name, Description, LocationURL, LatLon,InActive, ImageUrl) 
+Values('The Basement East','Hangout for live music by nationally touring acts, with a bar for refreshments.',
+'https://www.thebasementnashville.com/','POINT (36.175686 -86.7577787)', 0 ,'https://www.thebasementnashville.com/wp-content/uploads/2017/10/thebasementnashvillecom_header-logo.png')
+
+
+insert Into Locations (Name, Description, LocationURL, LatLon,InActive, ImageUrl) 
+Values('The 5 Spot','Snacks & a full bar served in a relaxed hangout that hosts live bands & weekly dance parties',
+'https://www.the5spot.club/?v=9a0d701a3b2a','POINT (36.1784111 -86.7525859)', 0 ,'')
+
+
+insert Into Locations (Name, Description, LocationURL, LatLon,InActive, ImageUrl) 
+Values('Altar`d State','Retailer of stylish clothing for young women, plus jewelry, shoes, accessories & decor items.',
+'https://www.altardstate.com/','POINT (36.1364046 -86.8009391)', 0 ,'')
+
+insert Into Locations (Name, Description, LocationURL, LatLon,InActive, ImageUrl) 
+Values('Parnassus Books','Parnassus Books is the independent bookstore for independent people located in Nashville, Tennessee. 
+Our shop has been co-owned by novelist Ann Patchett and her business partner, Karen Hayes,',
+'https://www.parnassusbooks.net/','POINT (36.1056244 -86.8153047)', 0 ,'')
+
+insert Into Locations (Name, Description, LocationURL, LatLon,InActive, ImageUrl) 
+Values('Melrose Billiard Parlor','Founded in 1944, Melrose Billiard Parlor is Nashville’s most iconic dive bar. 
+We’re a classic bar offering drinks at yesteryear prices, pool, ping pong, games, and classic bar food.',
+'https://dirtymelrose.com/','POINT (36.1220318 -86.7752649)', 0 ,'')
+
+insert Into Locations (Name, Description, LocationURL, LatLon,InActive, ImageUrl) 
+Values('East End United Methodist Church','We believe that Jesus’ message of unconditional love means that every human being is a child of God and is worthy of love, respect and inclusion.',
+'https://eastendumc.org/','POINT (36.1761897 -86.7497556)', 0 ,'')
